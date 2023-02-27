c0e67ff683c17eb53448d617173e62726bc85dab
Oleksandr Gneushev <galexing@gmail.com>
2023-02-27 19:46:23 +0100

    create dashboard components

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { useList } from "@pankod/refine-core";
+import {
+  PieChart,
+  PropertyReferrals,
+  PropertyCard,
+  TotalRevenue,
+  TopAgent,
+} from "../components";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Box>
+      <Typography fontWeight={700} fontSize={25} color='#11142d'>
+        Dashboard
+      </Typography>
+
+      <Box mt='20px' display='flex' flexWrap='wrap' gap={4}>
+        <PieChart
+          title='Properties for Sale'
+          value={684}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+          title='Properties for Rent'
+          value={684}
+          series={[60, 40]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+          title='Total customers'
+          value={5684}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+          title='Properties for Cities'
+          value={555}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+      </Box>
+
+      <Stack
+        gap={4}
+        mt='25px'
+        width='100%'
+        direction={{ xs: "column", lg: "row" }}
+      >
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
+    </Box>
+  );
 };
 
 export default Home;
